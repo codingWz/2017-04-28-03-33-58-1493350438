e0dc7fd25d1628c4a178522c7df6cd61bc963f52
codingWz <342325130@qq.com>
2017-05-05 13:07:22 +0800

    Update BowlingGame.java

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,79 @@
 public class BowlingGame {
 
     public int getBowlingScore(String bowlingCode) {
-        return 0;
+       
+		int score = 0;
+		String[] input = bowlingCode.split("\\|\\|");
+		String[] common = input[0].split("\\|");
+		String[] revise;
+		
+		//has extras chance
+		if (input.length > 1) {
+			
+			String last = input[1];			
+			revise = new String[common.length + last.length()];
+			
+			for (int i = 0;i < common.length;i ++) {
+				revise[i] = common[i];
+			}
+			
+			StringBuilder sb1 = new StringBuilder(last);
+			if (last.length() == 1) {
+				if (!last.equals("X")) {
+					sb1.append("-");
+					revise[10] = sb1.toString();
+				}
+			}else {
+				StringBuilder sb2 = new StringBuilder();
+				sb1 = new StringBuilder(last.substring(0,1));
+				sb2 = new StringBuilder(last.substring(1,2));
+				
+				if (!sb1.toString().equals("X")) {
+					revise[10] = last;
+				}else {
+					revise[10] = "X";
+					if (!sb2.toString().equals("X")) {
+						sb2.append("-");
+					}
+					revise[11] = sb2.toString();
+				}
+					
+			}
+			
+		}else {
+			revise = common;
+		}
+		
+		for (int i = 0;i < 10;i ++) {
+			//match strike
+			if (revise[i].length() == 1) {
+				score += 10;
+				if (revise[i + 1].length() == 1) {
+					score += 10;
+					if (revise[i + 2].length() == 1) {
+						score += 10;
+					}else {
+						score += addExtra1(revise[i + 2]);
+					}
+				}else {
+					score += addExtra2(revise[i + 1]);
+				}
+			}else {
+				//match spare
+				if (revise[i].contains("/")) {
+					score += 10;
+					if (revise[i + 1].length() == 1) {
+						score += 10;
+					}else {
+						score += addExtra1(revise[i + 1]);
+					}
+				}else {//match common case
+					score += addExtra2(revise[i]);
+				}
+			}
+		}
+		
+		return score;
+	
     }
 }
